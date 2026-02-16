cefcc361e6d1bbf3d7b02fd0c5c8cacea7897c6d
Valisoa Nomena Loic Randrianaivo <varandri@student.42antananarivo.mg>
2026-02-16 09:47:38 +0300

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 09:42:15 by varandri          #+#    #+#             */
+/*   Updated: 2026/02/16 09:44:12 by varandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define FET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
