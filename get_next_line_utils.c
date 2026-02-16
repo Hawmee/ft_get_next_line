42ea111d638c05dc08390e02a0091daa97fc9be0
Valisoa Nomena Loic Randrianaivo <varandri@student.42antananarivo.mg>
2026-02-16 17:10:11 +0300

    next_line_utils : perfect

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 09:42:04 by varandri          #+#    #+#             */
-/*   Updated: 2026/02/16 16:57:19 by varandri         ###   ########.fr       */
+/*   Updated: 2026/02/16 17:07:39 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		while (n--)
 			*(d++) = *(s++);
 	else if (d > s)
-		while (n--)
-			d[n] = s[n];
+		while (n)
+		{
+			d[n - 1] = s[n - 1];
+			n--;
+		}
 	return (dest);
 }
 
@@ -67,8 +70,10 @@ char	*ft_strjoin(char *s1, char *s2)
 	res = (char *)ft_calloc(res_len + 1, sizeof(char));
 	if (!res)
 		return (NULL);
-	ft_memmove(res, s1, ft_strlen(s1));
-	ft_memmove(res + ft_strlen(s1), s2, ft_strlen(s2));
+	if (s1)
+		ft_memmove(res, s1, ft_strlen(s1));
+	if (s2)
+		ft_memmove(res + ft_strlen(s1), s2, ft_strlen(s2));
 	return (res);
 }
 
@@ -81,7 +86,7 @@ char	*ft_substr(char *s, size_t start, size_t len)
 		return (NULL);
 	if (start >= ft_strlen(s))
 	{
-		sb = ft_strjoin("",NULL);
+		sb = ft_calloc(1, sizeof(char));
 		return (sb);
 	}
 	if (len > ft_strlen(s) - start)
